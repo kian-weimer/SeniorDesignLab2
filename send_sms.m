ad7bc47c3b991b3745d0259bcfd1ec2578b2fcb0
Luke Pearson <l.pearson.vzw@gmail.com>
2021-10-06 19:05:22 -0500

    Add matlab code for sending sms message.

---
+++ send_sms.m
@@ -0,0 +1,60 @@
+function send_sms(number,carrier,subject,message)
+% SEND_TEXT_MESSAGE send text message to cell phone or other mobile device.
+%    SEND_TEXT_MESSAGE(NUMBER,CARRIER,SUBJECT,MESSAGE) sends a text message
+%    to mobile devices in USA. NUMBER is your 10-digit cell phone number.
+%    CARRIER is your cell phone service provider, which can be one of the
+%    following: 'Alltel', 'AT&T', 'Boost', 'Cingular', 'Cingular2',
+%    'Nextel', 'Sprint', 'T-Mobile', 'Verizon', or 'Virgin'. SUBJECT is the
+%    subject of the message, and MESSAGE is the content of the message to
+%    send.
+% 
+%    Example:
+%      send_text_message('234-567-8910','Cingular', ...
+%         'Calculation Done','Don't forget to retrieve your result file')
+%      send_text_message('234-567-8910','Cingular', ...
+%         'This is a text message without subject')
+%
+%
+% =========================================================================
+mail = 'THISNEEDSTOBESET@gmail.com';    %Your GMail email address
+password = 'THIS_IS_NOT_A_PASSWORD';          %Your GMail password
+% =========================================================================
+if nargin == 3
+    message = subject;
+    subject = '';
+end
+% Format the phone number to 10 digit without dashes
+number = strrep(number, '-', '');
+if length(number) == 11 && number(1) == '1'
+    number = number(2:11);
+end
+% Information found from
+% http://www.sms411.net/2006/07/how-to-send-email-to-phone.html
+switch strrep(strrep(lower(carrier),'-',''),'&','')
+    case 'alltel';    emailto = strcat(number,'@message.alltel.com');
+    case 'att';       emailto = strcat(number,'@mmode.com');
+    case 'boost';     emailto = strcat(number,'@myboostmobile.com');
+    case 'cingular';  emailto = strcat(number,'@cingularme.com');
+    case 'cingular2'; emailto = strcat(number,'@mobile.mycingular.com');
+    case 'nextel';    emailto = strcat(number,'@messaging.nextel.com');
+    case 'sprint';    emailto = strcat(number,'@messaging.sprintpcs.com');
+    case 'tmobile';   emailto = strcat(number,'@tmomail.net');
+    case 'verizon';   emailto = strcat(number,'@vtext.com');
+    case 'virgin';    emailto = strcat(number,'@vmobl.com');
+end
+% Set up Gmail SMTP service.
+% Note: following code found from
+% http://www.mathworks.com/support/solutions/data/1-3PRRDV.html
+% If you have your own SMTP server, replace it with yours.
+% Then this code will set up the preferences properly:
+setpref('Internet','E_mail',mail);
+setpref('Internet','SMTP_Server','smtp.gmail.com');
+setpref('Internet','SMTP_Username',mail);
+setpref('Internet','SMTP_Password',password);
+% Send the email
+sendmail(emailto,subject,message)
+
+%warn user if their email and password are not set
+if strcmp(mail,'THISNEEDSTOBESET@gmail.com')
+    disp('Please provide your gmail account address and password above.')
+end
